298cd11e5e1018e27f191901f8832082c54d8475
victor-sarti <sartivictor24@gmail.com>
2024-07-10 17:55:35 -0300

    import sections in the app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
 import React from 'react'
 import NavBar from './components/NavBar'
 import HeroSection from './components/HeroSection'
+import FeatureSection from './components/Recurse'
+import Location from './components/Location'
+import Footer from './components/Footer'
+import Testimonials from './components/Depoiments'
 const App = () => {
   return (
       <>
       <NavBar/>
       <div className="max-w-7xl mx-auto pt-20 px-6">
     <HeroSection/>
+    <FeatureSection/>
+    <Testimonials/>
+    <Location/>
+   <Footer/>
     
     </div>
       </>
