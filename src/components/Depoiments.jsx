2c22f2816989d4ff5f2e0874d63c3981267b7d24
victor-sarti <sartivictor24@gmail.com>
2024-07-12 16:45:03 -0300

    remove the images depoiments

--- src/components/Depoiments.jsx
+++ src/components/Depoiments.jsx
@@ -10,14 +10,9 @@ const Testimonials = () => {
                     <div key={index} className="w-full sm:w-1/2 lg:w-1/3 px-4 py-2">
                         <div className="bg-neutral rounded-md p-6 text-md border border-neutral-800 font-thin">
                             <p> {testimonials.text} </p>
-                            <div className="flex mt-8 items-start">
-                                <img className="w-12 h-12 mr-6 rounded-full border border-neutral-300"
-                                    src={testimonials.image}
-                                    alt={testimonials.user} />
-                                <div>
+                            <div className="flex  flex-col mt-8 items-start">
                                     <h6>{testimonials.user}</h6>
-                                    <span className=" text-sm font-normal italic text-neutral-600">{testimonials.company}</span>
-                                </div>
+                                    <span className=" text-sm font-normal italic text-neutral-600">{testimonials.company}</span>      
                             </div>
                         </div>
                     </div>
