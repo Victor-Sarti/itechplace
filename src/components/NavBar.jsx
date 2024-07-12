42ea1dc55cf97678ea189a5988faba0abc1491b9
victor-sarti <sartivictor24@gmail.com>
2024-07-12 16:55:51 -0300

    feat: import animation/hover for buttons

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,6 +2,7 @@ import {Menu, X} from "lucide-react"
 import { useState } from "react";
 import logoi from "../assets/logoi.png";
 import { navItems } from "../constants";
+import './hero-animations.css';
 const NavBar = () => {
     const [moblieDrawerOpen, setMoblieDrawerOpen] = useState(false);
     const toggleNavbar = ( ) => {
@@ -15,16 +16,16 @@ const NavBar = () => {
                     <img className="h-10 w-10 mr-2" src={logoi} alt="logo" />
                     <span className="text-xl tracking-tight">itechplace</span>
                 </div>
-                <ul className=" hidden lg:flex ml-14 space-x-12">      
+                <ul className=" hidden lg:flex ml-14 space-x-12 ">      
                 {navItems.map((item, index) => (
                     <li key={index}>
-                        <a href={item.href}>{item.label}</a>
+                        <a className="hover:text-blue-500" href={item.href}>{item.label}</a>
                     </li>
                 ))}
                 </ul>
                 <div className=" hidden lg:flex justify-center space-x-12 items-center">
-                    <a href="https://www.instagram.com/itechplace?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="py-2 px-3 border rounded-md">Instagram </a>
-                    <a href="https://api.whatsapp.com/send/?phone=5511976247505&text&type=phone_number&app_absent=0wa.me/5511976247505" className="bg-gradient-to-r from-blue-500 to-blue-800 py-2 px-3 rounded-md"> Fale Conosco Aqui</a>
+                    <a href="https://www.instagram.com/itechplace?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw==" className="py-2 px-3 border rounded-md button-scale-on-hover hover:text-blue-500 hover:border-blue-500">Instagram </a>
+                    <a href="https://api.whatsapp.com/send/?phone=5511976247505&text&type=phone_number&app_absent=0wa.me/5511976247505" className="bg-gradient-to-r from-blue-500 to-blue-800 py-2 px-3 rounded-md button-scale-on-hover hover:text-stone-950"> Fale Conosco Aqui</a>
                 </div>
 
                 <div className="lg:hidden md:flex flex-col justify-end">
@@ -38,7 +39,7 @@ const NavBar = () => {
                             <ul>
                                 {navItems.map((item,index) => (
                                     <li key={index} className="py-4">
-                                        <a href={item.href}>{item.label}</a>
+                                        <a className="hover:text-blue-500" href={item.href}>{item.label}</a>
 
                                     </li>
                                 ))}
